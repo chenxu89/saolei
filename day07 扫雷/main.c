202d1f3af4630711f8ed9b289d673cc4eb149d9c
chenxu89 <675269196@qq.com>
2015-09-09 23:49:57 +0800

    Initial Commit

---
+++ day07 扫雷/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  day07 扫雷
+//
+//  Created by 陈旭 on 15/9/9.
+//  Copyright (c) 2015年 陈旭. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
